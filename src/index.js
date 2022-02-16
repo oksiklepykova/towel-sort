d9adaa59bab90dd32799fc9d876af321baecb8f4
oksiklepykova <oksiklepykova@gmail.com>
2022-02-16 19:24:12 +0300

    feat:  add completed task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+        if (matrix === undefined) return [];
+        let arr = [];
+        matrix.map((cur, index) => {
+          if ((index + 1) % 2 === 0) {
+            cur = cur.reverse();
+          }
+          cur.map(el => {
+            arr.push(el);
+          });
+        });
+          return arr;
 }
